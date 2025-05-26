934a0e80c03913d7cde9d9ac0154a8f279b49813
djinylh <d.jinylh@gmail.com>
2025-05-26 18:04:36 +0900

    [docs] 복습

---
+++ src/ch02/sec12/PrintfExample.java
@@ -0,0 +1,41 @@
+package ch02.sec12;
+
+public class PrintfExample {
+    public static void main(String[] args) {
+        int value = 12300;
+        System.out.printf("상품의 가격:%,d원\n", value);
+
+        String result1 = String.format("상품의 가격:%,d원\n", value);
+        System.out.println(result1);
+
+
+        System.out.printf("상품의 가격:%11d원\n", value);
+        System.out.printf("상품의 가격:%-11d원\n", value);
+        System.out.printf("상품의 가격:%011d원\n", value);
+
+
+
+
+
+        int year = 2025;
+        int mon  =8;
+        int day = 20;
+        //2025-09-04
+        System.out.printf("%4d-%02d-%02d\n",year,mon ,day);
+
+        int radius = 10;
+        double area = 3.14159 * radius * radius; //반지름 10의 원의 넓이
+        System.out.printf("반지름이 %d인 원의 넓이:%f입니다.\n", radius,area);
+        System.out.printf("반지름이 %d인 원의 넓이:%10.2f입니다.\n", radius,area);
+        System.out.printf("반지름이 %d인 원의 넓이:%-10.2f입니다.\n", radius,area);
+        System.out.printf("반지름이 %d인 원의 넓이:%010.2f입니다.\n", radius,area);
+
+
+        String name = "홍길동";
+        String job = "도적";
+        System.out.printf("%6d|%-10s|%10s\n", 1,name, job);
+
+
+    }
+
+}
---
+++ src/ch03/sec01/ArithmeticOperatorExample.java
@@ -0,0 +1,22 @@
+package ch03.sec01;
+
+public class ArithmeticOperatorExample {
+    public static void main(String[] args) {
+        //산술연산자
+        int v1 = 10, v2 = 4, v3 =5;
+        long v4 = 10L;
+
+
+
+        int result1 = v1 + v2;
+
+
+
+        System.out.println("result1: " + result1); // 14
+
+        System.out.println("reseult2: " + (v1 + v2 - v4)); // 4
+
+
+        System.out.println("result3 " + ((double)v1/v2));
+    }
+}
